7ab59fdc4d31566a0b195bca508e42cb68854569
Erik Brännström <erik.brannstrom@timeedit.se>
2015-02-23 11:28:15 +0100

    Refactor RPC response parsing as a stream transform.

--- index.js
+++ index.js
@@ -1,59 +1,9 @@
 var net = require('net');
+var RPCTransform = require('./lib/RPCTransform');
 
 var client = net.connect({ host: '172.16.10.4', port: 9001 });
 
-var response = null;
-var position = 0;
-var is64BitResponse = false;
-client.on('data', function (data) {
-	var offset = 0;
-
-	if (response === null) {
-		if (data.readUInt8(0) !== 173 || (data.readUInt8(3) !== 0 && data.readUInt8(3) !== 5)) {
-			throw new Error('Invalid response', data.toJSON());
-		}
-
-		if (data.readUInt8(3) === 5) {
-			is64BitResponse = true;
-		}
-
-		var responseLength = data.readUInt16LE(1);
-		response = new Buffer(responseLength);
-		offset = 4;
-
-		if (data.length <= offset) {
-			return;
-		}
-	}
-
-	if (is64BitResponse === true) {
-		var contentLengthStr = data.toString('utf8', 2, data.readUInt16LE(0) + 2);
-		if (contentLengthStr !== "CONTENT_LENGTH") {
-			throw new Error('Invalid response', data.toJSON());
-		}
-		var length = 0;
-		var start = data.readUInt16LE(0) + 4;
-		var numDigits = response.length - start;
-		for (i = start; i < response.length; i++) {
-			length = length + (data.readUInt8(i) - 48) * Math.pow(10, numDigits - 1); // ASCII to int
-			numDigits = numDigits - 1;
-		}
-		offset = response.length;
-		response = new Buffer(length);
-		is64BitResponse = false;
-	}
-
-	data.copy(response, position, offset);
-	position = position + data.length - offset;
-
-	if (position === response.length) {
-		console.log(response.toString('utf8'));
-		client.end();
-	}
-});
-
 var call = function (name, args) {
-	var ulen = name.length;
 	var dataSize = (2 + name.length);
 	for (var i = 0; i < args.length; i++) {
 		dataSize += 2 + args[i].length;
@@ -69,7 +19,7 @@ var call = function (name, args) {
 	// Create data packet
 	var body = new Buffer(dataSize);
 	// Write function name to buffer
-	body.writeUInt16LE(ulen, 0);
+	body.writeUInt16LE(name.length, 0);
 	body.write(name, 2, name.length, 'utf8');
 
 	// Write arguments to buffer
@@ -83,6 +33,7 @@ var call = function (name, args) {
 	// Send data
 	client.write(header);
 	client.write(body);
+	var stream = client.pipe(new RPCTransform()).pipe(process.stdout);
 };
 
 // CLI
